33e3f630366b96ae0b443c04ab20c769e5e8b544
tomataso <tomataso@users.noreply.github.com>
2020-11-21 15:01:42 -0600

    Todo hasta el paso 12

--- ContactsAPI/src/main/java/com/cenfotec/contacts/controllers/ContactController.java
+++ ContactsAPI/src/main/java/com/cenfotec/contacts/controllers/ContactController.java
@@ -14,56 +14,48 @@ import com.cenfotec.contacts.model.Contact;
 import com.cenfotec.contacts.repository.ContactRepository;
 
 @RestController
-@RequestMapping({"/contacts"})
+@RequestMapping({ "/contacts" })
 public class ContactController {
-	
- private ContactRepository repository;
- 
- ContactController(ContactRepository contactRepository) {
- this.repository = contactRepository;
- }
- 
- @GetMapping
- public List findAll(){
-  return repository.findAll();
- }
 
- @GetMapping(path = {"/{id}"})
- public ResponseEntity<Contact> findById(@PathVariable
- long id){
-  return repository.findById(id)
-  .map(record ->
- ResponseEntity.ok().body(record))
-  .orElse(ResponseEntity.notFound().build());
- }
- 
- @PostMapping
- public Contact create(@RequestBody Contact contact){
-  return repository.save(contact);
- }
+	private ContactRepository repository;
 
- @PutMapping(value="/{id}")
- public ResponseEntity<Contact>
-update(@PathVariable("id") long id,
- @RequestBody Contact contact){
- return repository.findById(id)
- .map(record -> {
- record.setName(contact.getName());
- record.setEmail(contact.getEmail());
- record.setPhone(contact.getPhone());
- Contact updated = repository.save(record);
- return ResponseEntity.ok().body(updated);
- }).orElse(ResponseEntity.notFound().build());
- }
- 
- @DeleteMapping(path ={"/{id}"})
- public ResponseEntity<?> delete(@PathVariable("id")
-long id) {
- return repository.findById(id)
- .map(record -> {
- repository.deleteById(id);
- return ResponseEntity.ok().build();
- }).orElse(ResponseEntity.notFound().build());
- }
- 
+	ContactController(ContactRepository contactRepository) {
+		this.repository = contactRepository;
+	}
+
+	@GetMapping
+	public List findAll() {
+		return repository.findAll();
+	}
+
+	@GetMapping(path = { "/{id}" })
+	public ResponseEntity<Contact> findById(@PathVariable long id) {
+		return repository.findById(id).map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public Contact create(@RequestBody Contact contact) {
+		return repository.save(contact);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Contact> update(@PathVariable("id") long id, @RequestBody Contact contact) {
+		return repository.findById(id).map(record -> {
+			record.setName(contact.getName());
+			record.setEmail(contact.getEmail());
+			record.setPhone(contact.getPhone());
+			Contact updated = repository.save(record);
+			return ResponseEntity.ok().body(updated);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping(path = { "/{id}" })
+	public ResponseEntity<?> delete(@PathVariable("id") long id) {
+		return repository.findById(id).map(record -> {
+			repository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
 }
