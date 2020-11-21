1a47429540ffe2d390f3293aadc6468150fc2529
tomataso <tomataso@users.noreply.github.com>
2020-11-21 14:42:53 -0600

    Hasta el paso 6 con exito

---
+++ ContactsAPI/src/main/java/com/cenfotec/contacts/model/Contact.java
@@ -0,0 +1,22 @@
+package com.cenfotec.contacts.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contact {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long id;
+ private String name;
+ private String email;
+ private String phone;
+}
